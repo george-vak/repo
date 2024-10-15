f06006fdc39c1edbd679574c5f3ed22b456888b0
Vakulenko Georgy <g.vakulenko.s70@gmail.com>
2024-10-15 11:58:29 +0300

    para

---
+++ 20241015/0/8.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+main = input().split()
+small = input().split()
+
+m = Counter(main)
+s = Counter(small)
+
+
+
+print("True") if s <= m else print("False")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
