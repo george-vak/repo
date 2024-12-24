5467e5c75feae98dee2a123d52ef78642f32ca24
Vakulenko Georgy <g.vakulenko.s70@gmail.com>
2024-12-24 11:03:58 +0300

    -

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ find "$BASE_DIR" -type d -name "check" | grep "2024" | while read -r CHECK_DIR; 
     RELATIVE_PATH=$(cd "$CHECK_DIR" && pwd | sed "s|$BASE_DIR/||" | sed 's|/check$||')
     cat << EOF > "$FILE_PATH"
 [remote]
+"$HIM:$RELATIVE_PATH/0" = []
 "$HIM:$RELATIVE_PATH/1" = []
 "$HIM:$RELATIVE_PATH/2" = []
-"$HIM:$RELATIVE_PATH/3" = []
 EOF
 
     echo "Файл создан: $FILE_PATH"
